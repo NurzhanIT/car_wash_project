43031170c369041592fb4dd125dc4ee4ec32ef81
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-11-22 03:24:15 +0600

    новая модель id

--- car_wash/car/models.py
+++ car_wash/car/models.py
@@ -1,19 +1,16 @@
+import torch.hub
 from django.db import models
 
 
 class Video(models.Model):
-    video = models.FileField(upload_to='video/videos/')
+    video = models.FileField(upload_to='video/videos/', primary_key=True)
     date = models.DateField(auto_now_add=False)
     carNum = models.IntegerField()
 
 
-    # def __str__(self):
-    #     return self.body[0:50]
-
-
 class Car(models.Model):
-
     date = models.DateField(auto_now_add=False)
     arrived_time = models.TimeField()
     left_time = models.TimeField()
     video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    id = models.IntegerField(max_length=1000)
