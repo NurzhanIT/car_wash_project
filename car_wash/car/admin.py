076cb467a856648b1740a5fafdbf026e3f3d7925
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2022-11-24 06:39:45 +0600

    переделка car/models.py, добавление нового приложения detectobj

--- car_wash/car/admin.py
+++ car_wash/car/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Video, Car
+from .models import VideoSet, Car
 
-admin.site.register(Video)
+admin.site.register(VideoSet)
 admin.site.register(Car)
