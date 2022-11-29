d535ce3e8c64158d7d5639ebb660adb795f1d79b
Nurzhan <mrb.nurzhan@gmail.com>
2022-11-29 15:37:53 +0600

    fixed serializer

--- car_wash/video_set/serializers.py
+++ car_wash/video_set/serializers.py
@@ -3,48 +3,60 @@ from rest_framework import serializers
 from video_set.models import VideoSet, VideoFile, Car
 
 
-class VideoSetSerializer(serializers.ModelSerializer):
-    # videos = serializers.SerializerMethodField()
-    set_date = serializers.SerializerMethodField()
-    cars_quantity = serializers.SerializerMethodField()
 
+from video_set.models import VideoSet, VideoFile, Car
+
+
+class CarSerializer(serializers.ModelSerializer):
     class Meta:
-        model = VideoSet
-        fields = ['set_name', 'set_date', 'cars_quantity']
-
-    @staticmethod
-    def get_set_date(request):
-        set_date = request.set_date
-        return set_date
-
-    @staticmethod
-    def get_cars_quantity(related_video):
-        return Car.objects.filter(related_video=related_video).count()
-
-    # def get_videos(self, video_set):
-    #     video_info = VideoFile.objects.filter(video_set=video_set).values_list('video'), VideoFile.objects.filter(
-    #         video_set=video_set).values_list('video', 'car').count()
-    #
-    #     return video_info
-
-
-# class CarSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Car
-#         fields = '__all__'
+        model = Car
+        fields = '__all__'
 
 
 class VideoSerializer(serializers.ModelSerializer):
     cars_quantity = serializers.SerializerMethodField()
+    car_set = CarSerializer(many=True, read_only=True)
 
     class Meta:
         model = VideoFile
-        fields = ['video', 'cars_quantity']
+        fields = ['video', 'car_set', 'cars_quantity']
 
-    @staticmethod
-    def get_cars_quantity(related_video):
+    def get_cars_quantity(self, related_video):
         return Car.objects.filter(related_video=related_video).count()
 
-    # def get_set_interval(self, request):
-    #     set_interval = 'c %s до %s' % (request.set_start_date, request.set_end_date)
-    #     return set_interval.strip()
+
+class VideoSetSerializer(serializers.ModelSerializer):
+    videofile_set = VideoSerializer(many=True, read_only=True)
+
+
+    class Meta:
+        model = VideoSet
+        fields = ['set_name', 'set_date', 'videofile_set']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
